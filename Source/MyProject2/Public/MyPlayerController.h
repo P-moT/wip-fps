daae878dc8539db6de4291021e3ba6a7b1c7f1a9
Thomas Peltier <tommypeltier@gmail.com>
2022-12-21 00:17:34 -0500

    Initial commit

---
+++ Source/MyProject2/Public/MyPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AMyPlayerController : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};
